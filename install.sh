ca4f5e93726c5d63b6258a730abe2e11ea4ad6c7
naominix <legal.h2@gmail.com>
2014-06-16 00:10:35 +0900

    Added download Sample Scratch Projects

--- install.sh
+++ install.sh
@@ -80,6 +80,92 @@ else
     wget -P "/home/pi/Documents/Scratch Projects" http://scratch2mcpi.github.io/scratch_projects/mcpi_stagescanner.sb
 fi
  
+# Download MinecraftGraphicsTurtle and Stuff sample Scratch projects
+echo "\n\033[36m\033[1mDownloading MinecraftGraphicsTurtle and Sutff sample Scratch projects...\033[00m\n"
+if [ -f "/home/pi/Documents/Scratch Projects/3dnautilus.sb" ]; then
+    echo "\n\033[33m\033[1m3dnautilus.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/3dnautilus.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/circle1.sb" ]; then
+    echo "\n\033[33m\033[1mcircle1.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/circle1.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/circle2.sb" ]; then
+    echo "\n\033[33m\033[1mcircle2.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/circle2.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/circle3.sb" ]; then
+    echo "\n\033[33m\033[1mcircle3.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/circle3.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/face1.sb" ]; then
+    echo "\n\033[33m\033[1mface1.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/face1.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/line1.sb" ]; then
+    echo "\n\033[33m\033[1mline1.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/line1.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/pattern.sb" ]; then
+    echo "\n\033[33m\033[1mpattern.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/pattern.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/pattern2.sb" ]; then
+    echo "\n\033[33m\033[1mpattern2.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/pattern2.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/pattern3.sb" ]; then
+    echo "\n\033[33m\033[1mpattern3.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/pattern3.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/spiral.sb" ]; then
+    echo "\n\033[33m\033[1mspiral.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/spiral.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/spiral2.sb" ]; then
+    echo "\n\033[33m\033[1mspiral2.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/spiral2.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/star1.sb" ]; then
+    echo "\n\033[33m\033[1mstar1.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/star1.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/star2.sb" ]; then
+    echo "\n\033[33m\033[1mstar2.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/star2.sb
+fi
+
+if [ -f "/home/pi/Documents/Scratch Projects/stuff1.sb" ]; then
+    echo "\n\033[33m\033[1mstuff1.sb exists. Skipped downloading.\033[00m\n"
+else
+    wget -P "/home/pi/Documents/Scratch Projects" http://naominix.github.io/scratch_projects/stuff1.sb
+fi
+
 # Copy Scratch2MCPI shortcut on Desktop
 if [ -f /home/pi/Desktop/scratch2mcpi.desktop ]; then
     echo "\n\033[33m\033[1mScratch2MCPI shortcut on Desktop exists. Skipped copying.\033[00m\n"
@@ -90,10 +176,10 @@ fi
  
 # Copy Scratch2MCPI Terminal shortcut on Desktop
 if [ -f /home/pi/Desktop/scratch2mcpi_terminal.desktop ]; then
-    echo "\n\033[33m\033[1mScratch2MCPI Terminal shotcut on Desktop exists. Skippted copying.\033[00m\n"
+    echo "\n\033[33m\033[1mScratch2MCPI Terminal shortcut on Desktop exists. Skipped copying.\033[00m\n"
 else
     echo "\n\033[36m\033[1mCopying Scratch2MCPI Terminal shortcut on Desktop...\033[00m\n"
     wget -P "/home/pi/Desktop" http://scratch2mcpi.github.io/scratch2mcpi_terminal.desktop
 fi
-
+ 
 echo "\n\033[32m\033[1mInstallation of scratch2mcpi is completed.\033[00m\n"
