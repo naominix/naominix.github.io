99e9fba766bf40a0c3a3d7fca5efb365e802cf23
naominix <legal.h2@gmail.com>
2014-07-21 12:08:36 +0900

    Change Folder and Repository name.
    Change name from “eVY14RasPi” to “eVY1RasPi”.

--- pokemikuinstall.sh
+++ pokemikuinstall.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
  
 SQUEAKDIR="/usr/lib/squeak/4.10.2-2793"
-MIDIPLUGIN="so.MIDIPlugin.140719"
+MIDIPLUGIN="so.MIDIPlugin.140720"
 SCRATCHDIR="/usr/share/scratch/locale"
 cd /home/pi
  
-# Install eVY14RasPi
+# Install eVY1RasPi
 echo "\n\033[36m\033[1mInstalling eVY1 Scratch on RaspberryPi for NSX-39(PokeMiku)...\033[00m\n"
-wget -P /tmp https://github.com/naominix/eVY14RasPi/archive/master.zip
+wget -P /tmp https://github.com/naominix/eVY1RasPi/archive/master.zip
 unzip /tmp/master.zip
-if [ -d /home/pi/eVY14RasPi ]; then
-    cp -rf eVY14RasPi-master/* eVY14RasPi/
-    rm -rf eVY14RasPi-master
+if [ -d /home/pi/eVY1RasPi ]; then
+    cp -rf eVY1RasPi-master/* eVY1RasPi/
+    rm -rf eVY1RasPi-master
 else
-    mv eVY14RasPi-master eVY14RasPi
+    mv eVY1RasPi-master eVY1RasPi
 fi
 rm /tmp/master.zip
  
 # Install MIDIPLUGIN File
 echo "\n\033[36m\033[1mInstalling MIDI Plugin File(Overwrite)...\033[00m\n"
 if [ -f $SQUEAKDIR/so.MIDIPlugin ]; then
-    sudo cp eVY14RasPi/$MIDIPLUGIN $SQUEAKDIR/so.MIDIPlugin
+    sudo cp eVY1RasPi/$MIDIPLUGIN $SQUEAKDIR/so.MIDIPlugin
 else
     echo "\n\033[33m\033[1m $SQUEAKDIR/so.MIDIPlugin Not Found. Please check Scratch install. Skipped copying.\033[00m\n"
 fi
@@ -28,7 +28,7 @@ fi
 # Install eVY1 locale file
 echo "\n\033[36m\033[1mInstalling eVY1 Scratch locale file(Overwrite)...\033[00m\n"
 if [ -d $SCRATCHDIR ]; then
-    sudo cp eVY14RasPi/ja_eVY1.po $SCRATCHDIR/
+    sudo cp eVY1RasPi/ja_eVY1.po $SCRATCHDIR/
 else
     echo "\n\033[33m\033[1mScratch directory Not Found. Please check Scratch install. Skipped copying.\033[00m\n"
 fi
@@ -38,7 +38,7 @@ if [ -f /home/pi/Desktop/eVY1.desktop ]; then
     echo "\n\033[33m\033[1meVY1 Scratch shotcut on Desktop exists. Skippted copying.\033[00m\n"
 else
     echo "\n\033[36m\033[1mCopying eVY1 Scratch shortcut on Desktop...\033[00m\n"
-    cp eVY14RasPi/eVY1.desktop /home/pi/Desktop/
+    cp eVY1RasPi/eVY1.desktop /home/pi/Desktop/
 fi
 
 echo "\n\033[32m\033[1mInstallation of eVY1 Scratch for Raspberry Pi is completed.\033[00m\n"
