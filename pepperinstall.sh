3ef50822c3a28834c07de92aac973b94c51e0bd0
naominix <legal.h2@gmail.com>
2016-05-03 15:21:53 +0000

    Trivial fix

--- pepperinstall.sh
+++ pepperinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 cd /home/pi/
-sudo touch /etc/apt/sources.list.d/chromium.list
-sudo cat "deb http://ppa.launchpad.net/canonical-chromium-builds/stage/ubuntu vivid main" > /etc/apt/sources.list.d/chromium.list
+cat "deb http://ppa.launchpad.net/canonical-chromium-builds/stage/ubuntu vivid main" > chromium.list
+sudo mv chromium.list /etc/apt/sources.list.d/chromium.list
 sudo apt-key adv -–keyserver keyserver.ubuntu.com -–recv-keys 5B393194
 sudo apt-get update
 sudo apt-get -y install chromium-browser chromium-browser-l10n
@@ -10,5 +10,7 @@ cd pepper
 wget http://os.archlinuxarm.org/armv7h/alarm/chromium-pepper-flash-12.0.0.77-1-armv7h.pkg.tar.xz
 tar Jxvf chromium-pepper-flash-12.0.0.77-1-armv7h.pkg.tar.xz
 sudo cp usr/lib/PepperFlash/* /usr/lib/chromium-browser/plugins
-sudo cat "CHROMIUM_FLAGS=\"–ppapi-flash-path=/usr/lib/chromium-browser/plugins/libpepflashplayer.so –ppapi-flash-version=12.0.0.77 -password-store=detect -user-data-dir\"" >> /etc/chromium-browser/default
+cp /etc/chromium-browser/default ./default
+cat "CHROMIUM_FLAGS=\"–ppapi-flash-path=/usr/lib/chromium-browser/plugins/libpepflashplayer.so –ppapi-flash-version=12.0.0.77 -password-store=detect -user-data-dir\"" >> default
+sudo mv default /etc/chromium-browser/default
 cd /home/pi
