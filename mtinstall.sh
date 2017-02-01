5f342391279553cc3907ec4535f3bb3459f126fd
naominix <legal.h2@gmail.com>
2017-02-01 09:20:52 +0900

    Changes to be committed: 	new file:   MineTools4maehara.zip 	new file:   mtinstall.sh Add MineTools Installer.

---
+++ mtinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+ 
+cd /home/pi
+ 
+# Install MineTools
+echo "\n\033[36m\033[1mInstalling MineTools...\033[00m\n"
+wget -P /tmp https://naominix.github.io/MineTools4maehara.zip
+unzip MineTools4maehara.zip
+#echo "\n\033[36m\033[1mCopying Scratch2MCPITurtle Terminal shortcut on Desktop...\033[00m\n"
+#        wget -P "/home/pi/Desktop" http://naominix.github.io/desktop/scratch2mcpiturtle_terminal.desktop
+ 
+echo "\n\033[32m\033[1mInstallation of MineTools is completed.\033[00m\n"
+fi
